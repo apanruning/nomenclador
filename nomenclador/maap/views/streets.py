aeeddacb10cafe559b31d71d565abd6ef8b64f05
etnalubma <francisco.herrero@gmail.com>
2009-10-19 16:49:57 -0200

    ups

--- nomenclador/maap/views/streets.py
+++ nomenclador/maap/views/streets.py
@@ -13,8 +13,7 @@ from osm.utils.words import clean_search_street
 
 from django.core import urlresolvers
 from django.utils.http import urlquote
-
-
+from django.contrib.gis.geos import LineString, MultiLineString, MultiPoint, Point
 
 def search_streets(request):
     streetname = clean_search_street(request.POST.get('streetname', ''))
@@ -28,20 +27,12 @@ def search_streets(request):
         if len(intlist) == 0:
             # This should be changed with elegant "not found streets"
             raise Http404
-        elif len(intlist) == 1:
-            # This should redirect to answer
-            args = '?str=%s&int=%s'%(urlquote(intlist[0][0]),urlquote(intlist[0][1]))
-            return redirect(urlresolvers.reverse(street_location)+args)
-
-    elif streetname and streetnumber and len(streetname)>2:
+            
+    elif streetname and len(streetname)>2:
         strlist = get_streets_list(streetname)
         if len(strlist) == 0:
             # This should be changed with elegant "not found streets"
             raise Http404
-        elif len(strlist) == 1:
-            # This should redirect to answer
-            args = '?str=%s&door=%s'%(urlquote(strlist[0][0]),streetnumber)
-            return redirect(urlresolvers.reverse(street_location)+args)
             
     context = RequestContext(request,{'intlist':intlist, 'strlist':strlist,'POST': request.POST})
     
@@ -51,56 +42,131 @@ def search_streets(request):
 def street_location(request):
     if request.method == 'GET':
         params = request.GET        
-        points = []
+
         if params.has_key('str'):
             if params.has_key('int'):
-                points += (get_locations_by_intersection(params['str'],params['int']))
+                # Intersection Case
+                points = get_locations_by_intersection(params['str'],params['int'])
+                layer = loc_int2layer(points, params['str'], params['int'])
                 
             elif params.has_key('door'):
+                # Street door Case
                 loc = get_location_by_door(params['str'],params['door'])
                 if loc:
-                    points.append(loc[0].wkt)
-            else:
-                raise Http404
-
-            if params.has_key('out'):
-                out = params['out']
-                if out == 'layer' and points:
-                    lpoints = []
-                    for p in points: 
-                        pgeom = OGRGeometry(p)
-                        pgeom.srs = 'EPSG:4326'
-                        pgeom.transform_to(SpatialReference('EPSG:900913'))
-                        pcoord = simplejson.loads(pgeom.json)
-                        lpoints.append({
-                          "type": "point",
-                          "id": 'point', 
-                          "name": "pipin",
-                          "geojson": pcoord, 
-                          "icon": {
-                            "url": "/media/icons/info.png", 
-                            "width": 32, 
-                            "height": 37
-                            }
-                        })
-                    
-                    layer = {
-                        'type': 'layer',
-                        'id': 'layer-location',
-                        'elements': lpoints,
-                        'box_size': None
-                    }                    
-                    return HttpResponse(simplejson.dumps(layer), mimetype='text/json')  
+                    layer = loc_door2layer(loc, params['str'],params['door'])                
                 else:
-                    raise Http404
-            
-            else:
-                path = request.get_full_path() + '&out=layer'
-                context = RequestContext(request, {'params': params, 'layerpath':path})
-                return render_to_response('maap/street_location.html', context_instance=context)               
+                    layer = loc_str2layer(params['str'])
                 
+            else:
+                # Street alone
+                layer = loc_str2layer(params['str'])
+                
+            return HttpResponse(layer, mimetype='text/json')  
+                            
         else:
             raise Http404        
     else:
         raise Http404
+
+
+def loc_str2layer(strn):
+    
+    swl = SearchableWay.objects.filter(name=strn)
+    wl = [sw.way for sw in swl]
+   
+    ln = []
+    for w in wl:
+        ln.append(LineString([u.node.geom for u in w.waynodes_set.all()]))
+    
+    ml = MultiLineString(ln)
+           
+    pgeom = OGRGeometry(ml.wkt)
+    pgeom.srs = 'EPSG:4326'
+    pgeom.transform_to(SpatialReference('EPSG:900913'))
+    lpoints = []
+    pcoord = simplejson.loads(pgeom.json)
+    lpoints.append({
+              "type": "multiline",
+              "id": 'street_%s' % strn,
+              "name": strn,
+              "geojson": pcoord,
+    
+            })
+        
+    layer = {
+            'type': 'layer',
+            'id': 'street_%s' % strn,
+            'elements': lpoints,
+            'box_size': None
+    }
+        
+    return HttpResponse(simplejson.dumps(layer), mimetype='text/json') 
+
+def loc_door2layer(pos, strn, door):
+    pgeom = OGRGeometry(pos[0].wkt)
+    pgeom.srs = 'EPSG:4326'
+    pgeom.transform_to(SpatialReference('EPSG:900913'))
+    pcoord = simplejson.loads(pgeom.json)
+
+    layer = {
+        'type': 'layer',
+        'id': "%s" % (strn),
+        'elements': [{
+          "type": "point",
+          "id": 'approx_location_%s_%s' % (strn, door), 
+          "name": "%s %s" % (strn, door),
+          "radius": pos[1],
+          "geojson": pcoord, 
+          "icon": {
+            "url": "/media/icons/info.png", 
+            "width": 32, 
+            "height": 37
+            }
+        }],
+        'box_size': pgeom.extent
+    }                
+    
+
+    return simplejson.dumps(layer)
+
+def loc_int2layer(points, str1, str2):
+    lpoints = []
+    i = 0
+    mp = None
+
+    for p in points: 
+
+        i += 1
+        pgeom = OGRGeometry(p)
+        pgeom.srs = 'EPSG:4326'
+        pgeom.transform_to(SpatialReference('EPSG:900913'))
+        pcoord = simplejson.loads(pgeom.json)
+
+        if not mp:
+            mp = pgeom
+        else:
+            mp.union(pgeom)
+                
+        lpoints.append({
+          "type": "point",
+          "id": 'intersection_%d'% i, 
+          "name": "%s y %s" % (str1, str2),
+          "geojson": pcoord, 
+          "icon": {
+            "url": "/media/icons/info.png", 
+            "width": 32, 
+            "height": 37
+            }
+        })
+    
+    layer = {
+        'type': 'layer',
+        'id': "%s_%s" % (str1, str2),
+        'elements': lpoints,
+        'box_size': mp.extent
+    }                    
+
+    return simplejson.dumps(layer)  
+
+
 
