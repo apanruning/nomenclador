aa72220bd9c40a40d2a97e217379505c05fd7289
Matias Iturburu <maturburu@gmail.com>
2010-04-25 22:40:28 -0300

    corregidos los templates y urls para usar registration 0.7

--- nomenclador/urls.py
+++ nomenclador/urls.py
@@ -7,7 +7,7 @@ from voting.views import vote_on_object
 from django.conf import settings 
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import views as auth_views
 
 from nomenclador.maap.models import MaapModel
 from nomenclador.profiles.forms import LoginForm
@@ -37,31 +37,22 @@ urlpatterns += patterns( '',
     (r'^contacto/$', 'nomenclador.profiles.views.mail', {}, 'mail'),
     )
 
+
 urlpatterns += patterns('',
-    url(r'^activate/complete/$',
-       direct_to_template,
-       {'template': 'registration/activation_complete.html'},
-       name='registration_activation_complete'),
+
     # Activation keys get matched by \w+ instead of the more specific
     # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
     # that way it can return a sensible "invalid key" message instead of a
     # confusing 404.
-    url(r'^activate/(?P<activation_key>\w+)/$',
-       'registration.views.activate',
-       {'backend': 'registration.backends.default.DefaultBackend'},
-       name='registration_activate'),
-    url(r'^register/$',
-       'registration.views.register',
-       {'backend': 'registration.backends.default.DefaultBackend'},
-       name='registration_register'),
-    url(r'^register/complete/$',
-       direct_to_template,
-       {'template': 'registration/registration_complete.html'},
-       name='registration_complete'),
-    url(r'^register/closed/$',
-       direct_to_template,
-       {'template': 'registration/registration_closed.html'},
-       name='registration_disallowed'),
+    url(r'^activate/(?P<activation_key>\w+)/$','registration.views.activate', name='registration_activate'),
+    url(r'^password/change/$', auth_views.password_change, name='auth_password_change'),
+    url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
+    url(r'^password/reset/$', auth_views.password_reset, name='auth_password_reset'),
+    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='auth_password_reset_confirm'),
+    url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
+    url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
+    url(r'^register/$', 'registration.views.register', name='registration_register'),
+    url(r'^register/complete/$', direct_to_template,  {'template': 'registration/registration_complete.html'},      name='registration_complete'),
     (r'^ingresar/$', 'nomenclador.profiles.views.login',{},'login'),
     (r'^salir/$', 'django.contrib.auth.views.logout', {}, 'logout'),
 )
