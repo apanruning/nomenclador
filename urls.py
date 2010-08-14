f2b46a686f1a3cdccabeab982d4aa8ceb8a3171e
Matias Iturburu <maturburu@gmail.com>
2010-08-14 17:26:12 -0300

    dejé andando el registro de usuarios

--- urls.py
+++ urls.py
@@ -40,19 +40,19 @@ urlpatterns += patterns( '',
 
 
 urlpatterns += patterns('',
-
+    url(r'^activate/complete/$',direct_to_template,{'template': 'registration/activation_complete.html'},name='registration_activation_complete'),
     # Activation keys get matched by \w+ instead of the more specific
     # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
     # that way it can return a sensible "invalid key" message instead of a
     # confusing 404.
-    url(r'^activate/(?P<activation_key>\w+)/$','registration.views.activate', name='registration_activate'),
+    url(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate', {'backend':'registration.backends.default.DefaultBackend'}, 'registration_activate'),
     url(r'^password/change/$', auth_views.password_change, name='auth_password_change'),
     url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
     url(r'^password/reset/$', auth_views.password_reset, name='auth_password_reset'),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='auth_password_reset_confirm'),
     url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
     url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
-    url(r'^register/$', 'registration.views.register', name='registration_register'),
+    (r'^register/$', 'registration.views.register', {'backend':'registration.backends.default.DefaultBackend'}, 'registration_register'),
     url(r'^register/complete/$', direct_to_template,  {'template': 'registration/registration_complete.html'},      name='registration_complete'),
     (r'^ingresar/$', 'profiles.views.login',{},'login'),
     (r'^accounts/login/', 'profiles.views.login',{},'login'),
