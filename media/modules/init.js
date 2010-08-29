4d9b0dd9c20c9f37d47653d06278b9a811f0fdf9
etnalubma <francisco.herrero@gmail.com>
2010-08-28 22:50:57 -0300

    Limpiando librerias obsoletas

--- media/modules/init.js
+++ media/modules/init.js
@@ -1,19 +1,22 @@
 /*** 
-    This module extends the state layer to set specific initialization functions to state 
+    This module extends the state layer to set specific initialization 
+    functions to state 
 ***/
 
-//this funcion hide what layers must be available in the switcher-layers control map when the map is shown
+// This funcion hide what layers must be available in the switcher-layers 
+// control map when the map is shown
 Maap.State.prototype.initializeBaseLayers = function() {
-    //layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-    layerMapnik = new OpenLayers.Layer.OSM.Mapnik("OpenStreetMap (Mapnik)",{layers: 'basic'});
+    layerMapnik = new OpenLayers.Layer.OSM.Mapnik(
+            "OpenStreetMap (Mapnik)",
+            {layers: 'basic'}
+    );
     this.map.addLayer(layerMapnik); 
     this.base_layers = this.base_layers.concat(layerMapnik);
-    //add here the layer that you want see in the switcher-layers control map when the map is shown. 
     
     return 0;
 }
 
-//this funcion set the initial bounds for map
+// This funcion set the initial bounds for map
 Maap.State.prototype.initializeBounds = function() {
     //change this values for change de level zoom
     this.map.zoomToExtent(
@@ -28,29 +31,19 @@ Maap.State.prototype.initializeBounds = function() {
     return 0;
 }
 
-//this function initialize the control objects of state 
+// This function initialize the control objects of state 
 Maap.State.prototype.initializeControls = function () {
     //add and/or remove controls to map
-    //this.map.addControl( new OpenLayers.Control.LayerSwitcher() );
-    //this.map.addControl( new OpenLayers.Control.MousePosition() );
-    this.map.addControl( new OpenLayers.Control.PanZoomBar() );
+    this.map.addControl(new OpenLayers.Control.PanZoomBar());
     this.map.addControl(new OpenLayers.Control.OverviewMap());
-
-    this.map.addControl( new OpenLayers.Control.Navigation() ); 
-    //this.map.addControl( new OpenLayers.Control.PanPanel() );
-    this.map.addControl( new OpenLayers.Control.Attribution() );
+    this.map.addControl(new OpenLayers.Control.Navigation()); 
+    this.map.addControl(new OpenLayers.Control.Attribution());
     return 0;
 }
 
 
-//this function initialize all settings of state. 
+// This function initialize all settings of state. 
 Maap.State.prototype.init = function() {
-    //Position: Cordoba
-    //var lon = -31.416667
-    //var lat = -64.183333
-    
-    //create a position indicating Cordoba
-    //cordoba = create_position(lon,lat);
 
     //add initial layers to map
     this.initializeBaseLayers();
