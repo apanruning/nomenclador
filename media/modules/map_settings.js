225a71e4494b10cf23e41b4910c8d1c51be0ca4f
“monetti” <onetti.martin@gmail.com>
2010-12-14 10:23:42 -0300

    agregue un modulo que me habia quedado si subir al repo. Vamos a ver si esto hace que el panel de alturas funcione en el servidor

---
+++ media/modules/map_settings.js
@@ -0,0 +1,10 @@
+//this module provide a certain of objects to config maps and other things relationated with the map of nomenclator. 
+//this, hide the implementations of many functions to manipulate things relationaed with maps
+
+
+
+//setting for the initial position center
+
+
+
+
