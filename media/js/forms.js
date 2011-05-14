c640e00709b342e9a8369a9b49e38472bf9e1eb9
tutuca <maturburu@gmail.com>
2011-05-14 17:02:20 -0300

    Mejoras al perfil y al formulario de login

--- media/js/forms.js
+++ media/js/forms.js
@@ -34,4 +34,31 @@ $(document).ready( function(){
     $('.search').submit(function(){
         $(this).find(':fields').noCrap()
     })
+    $( "#id_barrios" )
+		.autocomplete({
+			source: function( request, response ) {
+				$.getJSON( "/barrios/", {
+					tag_name: extractLast( request.term )
+				}, response );
+			},
+			search: function() {
+				var term = extractLast( this.value );
+				if ( term.length < 2 ) {
+					return false;
+				}
+			},
+			focus: function() {
+				return false;
+			},
+			select: function( event, ui ) {
+				var terms = split( this.value );
+				terms.pop();
+				terms.push( ui.item.value );
+				terms.push( "" );
+				this.value = terms.join( ", " );
+				return false;
+			}
+		});
+
 })
+
