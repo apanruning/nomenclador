1b7114bfbb48499379ef485f3188e18954634d08
tutuca <maturburu@gmail.com>
2011-05-14 01:32:09 -0300

    Se trabajó en el diseño, puliendo detalles donde se pudo. Los formularios y el menu de sesión es lo más destacable. También se corrijió la confifuración de tinymce para que sea más eficiente a la hora de limpiar el formato del texto.

--- urls.py
+++ urls.py
@@ -23,12 +23,13 @@ urlpatterns = patterns('maap.views',
     url(r'^category/(?P<cat_slug>[^/]+)$', 'search_places', name='list_by_category'),
     url(r'map/view/(?P<cat_slug>[^/]+)/(?P<object_id>\d+)$','view', name='view'),
     url(r'map/add/$','create', name='create'),
+    url(r'/publish/$','new_banner', name='publish'),
     (r'^map/streets/location$', 'street_location'),
     (r'^tags/(?P<tag>[^/]+)$','obj_list_by_tag'),
 
 )
 
-urlpatterns += patterns( '',
+urlpatterns += patterns('',
     (r'^comments/', include('django.contrib.comments.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
@@ -38,7 +39,7 @@ urlpatterns += patterns( '',
     (r'^contacto/$', 'profiles.views.mail', {}, 'mail'),
     # toolsnmt
     (r'^locations_tool/', include('toolsnmt.urls')),
-    )
+)
 
 
 urlpatterns += patterns('',
@@ -47,15 +48,15 @@ urlpatterns += patterns('',
     # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
     # that way it can return a sensible "invalid key" message instead of a
     # confusing 404.
-    url(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate', {'backend':'registration.backends.default.DefaultBackend'}, 'registration_activate'),
+    url(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate', {}, 'registration_activate'),
     url(r'^password/change/$', auth_views.password_change, name='auth_password_change'),
     url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
     url(r'^password/reset/$', auth_views.password_reset, name='auth_password_reset'),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='auth_password_reset_confirm'),
     url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
     url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
-    (r'^register/$', 'registration.views.register', {'backend':'registration.backends.default.DefaultBackend'}, 'registration_register'),
-    url(r'^register/complete/$', direct_to_template,  {'template': 'registration/registration_complete.html'},      name='registration_complete'),
+    (r'^register/$', 'registration.views.register', {}, 'registration_register'),
+    url(r'^register/complete/$', direct_to_template,  {'template': 'registration/registration_complete.html'}, name='registration_complete'),
     (r'^ingresar/$', 'profiles.views.login',{},'login'),
     (r'^accounts/login/', 'profiles.views.login',{},'login'),
     (r'^salir/$', 'maap.views.log_out', {}, 'logout'),
